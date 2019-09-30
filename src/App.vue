7991569cf0267d7909433968d60bcb2792bdd71a
DokySp <uhug@naver.com>
2019-09-30 23:07:35 +0900

    Modify UI

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,54 @@
 <template>
-  <v-app>
-    <v-app-bar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>ELECTRON </span>
-        <span class="font-weight-light">TEST APP</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn text href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank">
-        <v-icon>mdi-anchor</v-icon> &nbsp;
-        <!-- v-icon: https://vuetifyjs.com/ko/components/icons -->
-        <!-- icon forge: https://material.io/resources/icons/?icon=attachment&style=baseline -->
-        <span class="mr-2">최신 릴리즈</span>
-      </v-btn>
-    </v-app-bar>
-  
-    <v-content>
-      <HelloWorld/>
-    </v-content>
-  </v-app>
+    <v-app>
+        <v-app-bar app>
+            <v-toolbar-title class="headline text-uppercase">
+                <span>일렉트론 </span>
+                <span class="font-weight-light">APP</span>
+            </v-toolbar-title>
+            <v-spacer></v-spacer>
+
+            <v-btn v-on:click="chgView(0)" text>
+                HOME
+            </v-btn>
+            &nbsp;
+            <v-btn v-on:click="chgView(1)" text>
+                TEST
+            </v-btn>
+            &nbsp;
+
+            <v-btn text href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank">
+                <v-icon>mdi-anchor</v-icon> &nbsp;
+                <!-- v-icon: https://vuetifyjs.com/ko/components/icons -->
+                <!-- icon forge: https://material.io/resources/icons/?icon=attachment&style=baseline -->
+                <span class="mr-1">최신 릴리즈</span>
+            </v-btn>
+        </v-app-bar>
+    
+        <v-content>
+            <Test v-show="count == 1"></Test>
+            <HelloWorld v-show="count == 0"/>
+        </v-content>
+    </v-app>
 </template>
 
 <script>
-  import HelloWorld from './components/HelloWorld';
-  
-  export default {
-    name: 'App',
-    components: {
-      HelloWorld,
-    },
-    data: () => ({
-      //
-    }),
-  };
+    import HelloWorld from './components/HelloWorld';
+    import Test from './components/Test'
+    
+    export default {
+        name: 'App',
+        components: {
+            HelloWorld,
+            Test,
+        },
+        methods: {
+            chgView: function(input){
+                this.count = input
+                console.log(this.count + "/" + input)
+            }
+        },  
+        data: () => ({
+            count: 0,
+        }),
+    };
 </script>
--- src/components/Test.vue
+++ src/components/Test.vue
@@ -1,17 +1,36 @@
 <template>
     <v-layout row wrap>
-        <v-flex xs12>
-            
+        <v-flex xs12 sm4>
+            <v-chip color="red" text-color="white">
+                {{msg1}}
+            </v-chip>
+        </v-flex>
+        <v-flex xs12 sm4>
+            <v-chip color="orange" text-color="white">
+                {{msg2}}
+            </v-chip>
+        </v-flex>
+        <v-flex xs12 sm4>
+            <v-chip color="green" text-color="white">
+                {{msg3}}
+            </v-chip>
         </v-flex>
     </v-layout>
 </template>
 
 <script>
-export default {
-
-}
+    export default {
+        name: "test",
+        data() {
+            return {
+                msg1: "asdf",
+                msg2: "sasdf",
+                msg3: "asdf",
+            }
+        },
+    }
 </script>
 
 <style>
-
+    
 </style>
