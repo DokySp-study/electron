700f4cd26a81d953aade04abab4551b04b47994b
DokySp <uhug@naver.com>
2019-09-30 22:16:40 +0900

    UI 변경

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,43 @@
 <template>
-  <v-app>
-    <v-app-bar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        text
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
-    </v-app-bar>
-
-    <v-content>
-      <HelloWorld/>
-    </v-content>
-  </v-app>
+    <v-app>
+        <v-app-bar app>
+            <v-toolbar-title class="headline text-uppercase">
+                <span>ELECTRON </span>
+                <span class="font-weight-light">TEST</span>
+            </v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-btn text href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank">
+                <span class="mr-2">홈</span>
+            </v-btn>
+            &nbsp;
+            <v-btn text href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank">
+                <span class="mr-2">테스트</span>
+            </v-btn>
+            &nbsp;
+            <v-btn text href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank">
+                <span class="mr-2">릴리즈</span>
+            </v-btn>
+        </v-app-bar>
+    
+        <v-content>
+            <Test/>
+            <HelloWorld/>
+        </v-content>
+    </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld,
-  },
-  data: () => ({
-    //
-  }),
-};
+    import HelloWorld from './components/HelloWorld';
+    import Test from './components/Test'
+    
+    export default {
+        name: 'App',
+        components: {
+            HelloWorld,
+            Test,
+        },
+        data: () => ({
+            //
+        }),
+    };
 </script>
